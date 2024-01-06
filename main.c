83180e242ea94acd00b4d4e3d7a3ba446b48e482
root <root@DESKTOP-RMNR7JO>
2024-01-06 13:51:50 +0100

    Ustalenie kierunku ruchu mrówki w funkcji main, dodanie zabezpieczenia przed opuszczeniem planszy przez mrówkę.

--- main.c
+++ main.c
@@ -1,13 +1,26 @@
 #include "plansza.h"
+#include "logika.h"
 #include <stdio.h>
 
 int main() {
-    int width = 5;
-    int height = 5;
+    int width = 40;
+    int height = 40;
 
     Board *board = createBoard(width, height);
-    printf("Początkowa pozycja mrówki: (%d, %d)\n", board->antX, board->antY);
     
+    // Początkowy kierunek mrówki
+    board->antDirection = 0;
+    
+    printf("Początkowa pozycja mrówki: (%d, %d)\n", board->antX, board->antY);
+
+    for(int i = 0; i < 1000; i++) {
+        if(ruch(board) == -1) {
+            printf("Mrowka wyszla poza plansze!!!\n");
+            return -1;
+        };
+    }
+
+    // Wygląd planszy po 1000 iteracji:
     printBoard(board);
     freeBoard(board);
 
